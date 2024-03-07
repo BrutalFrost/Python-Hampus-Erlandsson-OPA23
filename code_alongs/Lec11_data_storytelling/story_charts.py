04113c647ed5ae35f66571cef8b0903b11f39c11
BrutalFrost <hampuser@net.chalmers.se>
2024-03-07 10:25:11 +0100

    set labels

--- code_alongs/Lec11_data_storytelling/story_charts.py
+++ code_alongs/Lec11_data_storytelling/story_charts.py
@@ -9,6 +9,29 @@ import pandas as pd
 DATA_PATH = Path(__file__).parents[2] / "data" / "data_processing"
 
 
+class StoryCharts:
+    def __init__(self) -> None:
+        pass
+
+    def _set_labels(self, title, xlabel, ylabel):
+        self.ax.set_xlabel(xlabel, loc="left")
+        self.ax.set_ylabel(ylabel, loc="top")
+        self.ax.set_title(title, loc="left", pad=15)
+
+
+    def _plot(self, x, y, color = "#0c4a6e", **label_kwargs):
+        self.fig, self.ax = plt.subplots()
+
+        self._set_labels(**label_kwargs)
+        plt.show()
+
+    def Line(self, x, y):
+        pass
+
+    def Bar(self, x, y):
+        pass
+
+
 
 # this is to be able to run this as a standalone script
 # the code below won't run when imported from another script
@@ -16,4 +39,11 @@ DATA_PATH = Path(__file__).parents[2] / "data" / "data_processing"
 
 if __name__ == "__main__":
     print("\n\n")
-    print(DATA_PATH)
+    #print(DATA_PATH)
+
+    df = pd.read_csv(DATA_PATH / "co2_annmean_mlo.csv", skiprows=43)
+
+    print(df.head())
+
+    sc = StoryCharts()
+    sc._plot(2, 3)
